70e11b09fcce6e376404879bf01eae70c5d87102
Kulindu Cooray <102121038+KulinduC@users.noreply.github.com>
2023-08-18 13:33:17 -0400

    Frontend fix (#32)
    * Frontend Adjustments
    
    * filtering done, search not so much
    
    * work hopefully again
    
    * FINAL PRES
    
    * FINAL PRES3
    
    * stuff
    
    * deliverable code
    
    ---------
    
    Co-authored-by: Isaac Efrosman <isaacefrosman3@gmail.com>
    Co-authored-by: Kulindu Cooray <kulinducooray@Kulindus-MacBook-Pro.local>
    Co-authored-by: Niels Mandrus <45430723+nmandrus1@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React , {useRef} from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import './login.css';
+import './index.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import logo from './retro_logo.png';
 
